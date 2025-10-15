42aeda153c899477b2f9032995044481fa57fdb2
eltrailero33 <a21490555@itmexicali.edu.mx>
2025-10-14 19:29:45 -0700

    le cambie la imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-//import 'package:chatsex_bogarin/screen/chat/chat_screen.dart';
+import 'package:chatsex_bogarin/screen/chat/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:chatsex_bogarin/config/theme/app_theme.dart';
-//import 'package:chatsex_bogarin/views/my_message_bubble_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +15,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColors: 2).theme(),
       home: Scaffold(
-        //appBar: AppBar(title: const Text('My SexBot')),
-        //body: const ChatScreen(),
+        appBar: AppBar(title: const Text('My ChatBot')),
+        body: const ChatScreen(),
       )
     );
   }
--- lib/screen/chat/chat_screen.dart
+++ lib/screen/chat/chat_screen.dart
@@ -11,7 +11,7 @@ class ChatScreen extends StatelessWidget{
         title: const Text('Mi Tilina'),
         centerTitle: false,
         leading: CircleAvatar(
-          backgroundImage: NetworkImage('https://scontent.felp1-1.fna.fbcdn.net/v/t39.30808-6/515631144_25021507870769496_6587686093632498510_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=0b6b33&_nc_eui2=AeF4zHvNAiH3dxda9KGjUbmxI_XZatlrd9Aj9dlq2Wt30Nwaaw5WZzpwThYe2Jn0tNrlZR3oQIonjBnuJLg1Sqaf&_nc_ohc=_ksL8CEE6cAQ7kNvwEWNvCM&_nc_oc=AdkY4n8j5fgwfgfjgdfUcsB_3rwaXt2RkMw0QFkjhqrPH9SxisF0OI2puuYtvDVKaj8&_nc_zt=23&_nc_ht=scontent.felp1-1.fna&_nc_gid=UgA-oPpa6zi3p3ZPGYBXnA&oh=00_AfeSJIpIEAzbku81bJZmzdQQMngDF8_sZRWzpnGCqFZbBg&oe=68EA4B94'),
+          backgroundImage: NetworkImage('https://generacionxbox.com/wp-content/uploads/2019/02/cortana-halo-infinite-generacionxbox.jpg'),
         ),
       ),
       body: ChatView(),
